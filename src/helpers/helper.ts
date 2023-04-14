f2db7a1fd03e8f888ad8b5ff349fc7a5101e88a8
Puzatka <ss88@tut.by>
2023-04-14 12:22:39 +0300

    service commit

--- src/helpers/helper.ts
+++
@@ -1,9 +0,0 @@
-export let CurrentDate = function () {
-    const now = new Date();
-      const months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
-      'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      let month: number|string = now.getMonth();
-      month = months[month];
-      let date = `${now.getDate()} ${month} ${now.getFullYear()}`; 
-      return date;
-  }
